d7e7e34b6d8b588a072df1add19bf59800ac1a49
Anurag <anurag.ghosh@research.iiit.ac.in>
2015-05-15 03:03:59 +0530

    Boilerplate.

--- src/detail/ms_api/document_impl.hpp
+++ src/detail/ms_api/document_impl.hpp
@@ -6,10 +6,14 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+#include <windows.h>
+#include <ole2.h>
+
 #include <boost/filesystem.hpp>
 #include <boost/document/detail/document_exception.hpp>
 #include <boost/document/detail/document_file_format.hpp>
 #include <boost/document/detail/document_interface.hpp>
+#include <boost/document/detail/ms_api/ms_functions.hpp>
 
 namespace boost { namespace detail { 
 
@@ -17,26 +21,31 @@ class ms_document: public document_interface {
 	
 	boost::filesystem::path doc_path_;
 	bool is_file_opened;	
+	CLSID clsid_;
+	IDispatch *appl_ptr_;
 
 	void initialize(const boost::filesystem::path& fpath) {
+		CoInitialize(NULL);
 		this->doc_path_ = fpath;
 		if(!boost::filesystem::exists(this->doc_path_)) {
         		boost::throw_exception(document_exception(
             		"Error: Path is empty or does not exist."));
     		}
+		this->clsid = boost::doc::ms_functions::get_clsid();
+		this->appl_ptr_ = boost::doc::ms_functions::get_application_pointer(clsid);
 		this->is_file_opened = false;
 	}
 
 	void open() {
-		if(not this->is_file_opened) {
-			//boost::doc::libre_functions::open_libre(this->doc_path_,this->xComponent_);
+		if(!this->is_file_opened) {
+			boost::doc::ms_functions::open_ms(this->doc_path_,this->appl_ptr_);
 			this->is_file_opened = true;
 		}
  	}
 
  	void close() {
  		if(this->is_file_opened) {
-			//boost::doc::libre_functions::close_libre(this->doc_path_, false, this->xComponent_);
+			boost::doc::ms_functions::close_ms(this->doc_path_, false, this->appl_ptr_);
 			this->is_file_opened = false;
 		}
 		else {
@@ -46,11 +55,11 @@ class ms_document: public document_interface {
  	}
  	
  	void save() {
- 		if(! this->is_file_opened) {
+ 		if(!this->is_file_opened) {
 			boost::throw_exception(document_exception(
             		"Error: Trying to save unopened file."));
 		}
- 		//boost::doc::libre_functions::save_libre(this->doc_path_, this->xComponent_);
+ 		boost::doc::ms_functions::save_ms(this->doc_path_, this->appl_ptr_);
  	}
  	
  	void save_as(const boost::filesystem::path& fpath) {
@@ -66,14 +75,16 @@ class ms_document: public document_interface {
  			boost::throw_exception(document_exception(
             		"Error: Trying to export unopened file."));
  		}
- 		//boost::doc::libre_functions::export_libre(this->doc_path_, format, this->xComponent_);
+ 		boost::doc::ms_functions::export_ms(this->doc_path_, format, this->appl_ptr_);
  	}
 
- 	~libre_document() {
+ 	~ms_document() {
  		if(this->is_file_opened) {
-			//boost::doc::libre_functions::close_libre(this->doc_path_, false, this->xComponent_);
+			boost::doc::ms_functions::close_ms(this->doc_path_, false, this->appl_ptr_);
 			this->is_file_opened = false;
 		}
+		CoUninitialize(NULL);
+
  	}
 
 };
--- src/detail/ms_api/ms_functions.cpp
+++ src/detail/ms_api/ms_functions.cpp
@@ -3,16 +3,26 @@
 #ifndef _MS_FUNCTIONS_CPP
 #define _MS_FUNCTIONS_CPP
 
-#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <cstdlib>
 #include <windows.h>
 #include <ole2.h>
 
-HRESULT AutoWrap(int autoType, VARIANT *pvResult, IDispatch *pDisp, LPOLESTR ptName, int cArgs...) {
+#include <boost/filesystem.hpp>
+#include <boost/throw_exception.hpp>
+
+#include <boost/document/detail/document_file_format.hpp>
+#include <boost/document/detail/libre_api/libre_functions.hpp>
+#include <boost/document/detail/document_exception.hpp>
+
+namespace boost { namespace doc { namespace ms_functions { 
+
+HRESULT auto_wrap_helper(int autoType, VARIANT *pvResult, IDispatch *pDisp, LPOLESTR ptName, int cArgs...) {
     va_list marker;
     va_start(marker, cArgs);
     if(!pDisp) {
-        MessageBox(NULL, "NULL IDispatch passed to AutoWrap()", "Error", 0x10010);
-        _exit(0);
+		boost::throw_exception(document_exception("Error: NULL IDispatch passed to AutoWrap()");
     }
     DISPPARAMS dp = { NULL, NULL, 0, 0 };
     DISPID dispidNamed = DISPID_PROPERTYPUT;
@@ -23,10 +33,8 @@ HRESULT AutoWrap(int autoType, VARIANT *pvResult, IDispatch *pDisp, LPOLESTR ptN
     WideCharToMultiByte(CP_ACP, 0, ptName, -1, szName, 256, NULL, NULL);
     hr = pDisp->GetIDsOfNames(IID_NULL, &ptName, 1, LOCALE_USER_DEFAULT, &dispID);
     if(FAILED(hr)) {
-        sprintf(buf, "IDispatch::GetIDsOfNames(\"%s\") failed w/err 0x%08lx", szName, hr);
-        MessageBox(NULL, buf, "AutoWrap()", 0x10010);
-        _exit(0);
-        return hr;
+		boost::throw_exception(document_exception(
+			"Error: IDispatch::GetIDsOfNames(" + std::string(szName) + ") failed w/err " +  std::to_string((int)hr));
     }
     VARIANT *pArgs = new VARIANT[cArgs+1];
     for(int i=0; i<cArgs; i++) {
@@ -40,14 +48,58 @@ HRESULT AutoWrap(int autoType, VARIANT *pvResult, IDispatch *pDisp, LPOLESTR ptN
     }
     hr = pDisp->Invoke(dispID, IID_NULL, LOCALE_SYSTEM_DEFAULT, autoType, &dp, pvResult, NULL, NULL);
     if(FAILED(hr)) {
-        sprintf(buf, "IDispatch::Invoke(\"%s\"=%08lx) failed w/err 0x%08lx", szName, dispID, hr);
-        MessageBox(NULL, buf, "AutoWrap()", 0x10010);
-        _exit(0);
-        return hr;
+		boost::throw_exception(document_exception(
+			"Error: IDispatch::GetIDsOfNames(" + std::string(szName) + "=" + 
+					std::to_string((int)dispID) + ") failed w/err " + std::to_string((int)hr));
     }
     va_end(marker);
     delete [] pArgs;   
     return hr;
 }
+
+CLSID get_clsid() {
+	CLSID clsid;
+	HRESULT hr = CLSIDFromProgID(L"Excel.Application", &clsid);
+	if(FAILED(hr)) {
+		boost::throw_exception(document_exception(
+		   "Error: CLSIDFromProgID() failed\n"));
+	}
+	return clsid;
+}
+
+void get_application_pointer(CLSID clsid, IDispatch *appl_ptr) {
+	HRESULT hr = CoCreateInstance(clsid, NULL, CLSCTX_LOCAL_SERVER, IID_IDispatch, (void **)&appl_ptr);
+	if (FAILED(hr)) {
+		boost::throw_exception(document_exception(
+			"Error: Excel not registered properly\n"));
+	}
+}
+
+void set_visibility(IDispatch *appl_ptr) {
+	VARIANT prop;
+	prop.vt = VT_I4;
+	prop.lVal = 1;
+	auto_wrap_helper(DISPATCH_PROPERTYPUT, NULL, appl_ptr, L"Visible", 1, prop);
+}
+
+void open_ms(const boost::filesystem::path& path, IDispatch *appl_ptr) {
+
+}
+
+void export_ms(const boost::filesystem::path& inputPath,
+	boost::document_file_format::type format,
+	IDispatch *appl_ptr) {
+	
+}
+
+void close_ms(const boost::filesystem::path &inputPath, bool save, IDispatch *appl_ptr) {
+
+}
+
+void save_ms(const boost::filesystem::path &inputPath, IDispatch *appl_ptr) {
+
+}
+
+}}}
 
 #endif
