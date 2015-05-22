3d0d3f932d439c56673a2e2daa7fae125b7c5a9a
Anurag <anurag.ghosh@research.iiit.ac.in>
2015-05-23 00:36:35 +0530

    Jamfile Edited.

--- build/has_ms_suit.cpp
+++ build/has_ms_suit.cpp
@@ -3,7 +3,7 @@
 // Boost Software License, Version 1.0. (See accompanying file
 // LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <stdio.h>
+#include <cstdio>
 #include <windows.h>
 #include <ole2.h>
 
@@ -86,7 +86,8 @@ int main() {
         AutoWrap(DISPATCH_PROPERTYGET, &result, pXlApp, L"Workbooks", 0);
         pXlBooks = result.pdispVal;
     }
-    IDispatch *pXlBook = NULL;
+
+	IDispatch *pXlBook = NULL;
     {
         VARIANT result;
         VariantInit(&result);
