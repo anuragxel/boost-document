6d1da37a4050b968834f3d19121d50776893813c
Anurag Ghosh <ghoshanurag1995@gmail.com>
2015-07-16 21:54:59 +0530

    Cleaning up Iterator Code #1

--- include/boost/document/sheet.hpp
+++ include/boost/document/sheet.hpp
@@ -20,14 +20,14 @@
 #include <cctype>
 
 namespace boost {
-    //! \brief This is the main class interface to be 
-    //!        exposed to the library user.
-    //!
+	//! \brief This is the main class interface to be 
+	//!        exposed to the library user.
+	//!
 	class sheet {
 	private:
 		std::shared_ptr<sheet_interface> pimpl_;
 		//boost::shared_ptr<sheet_interface> init() {
- 	   	//	return boost::detail::open_sheet_instance();
+		//	return boost::detail::open_sheet_instance();
 		//}
 	public:
 		//! \brief The Constructor.
@@ -43,33 +43,33 @@ namespace boost {
 
 		//! \brief Gets the sheet index
 		//!        which is being accessed.
-	 	std::size_t sheet_index() {
-	 		return pimpl_->sheet_index();
-	 	}
+		std::size_t sheet_index() {
+			return pimpl_->sheet_index();
+		}
 
 		//! \brief Renames the sheet to str
 		//!        which is being accessed.
-	 	void rename_sheet(const std::string& str) {
-	 		pimpl_->rename_sheet(str);
-	 	}
+		void rename_sheet(const std::string& str) {
+			pimpl_->rename_sheet(str);
+		}
 
 		//! Returns the maximum 
 		//! number of rows
-	 	std::size_t max_row() {
-	 		return pimpl_->max_row();
-	 	}
+		std::size_t max_row() {
+			return pimpl_->max_row();
+		}
 
 		//! Returns the maximum 
 		//! number of columns
-	 	std::size_t max_column() {
-	 		return pimpl_->max_column();
-	 	}
+		std::size_t max_column() {
+			return pimpl_->max_column();
+		}
 
-	 	//! Gets the cell instance
-	 	//! which can be manipulated.
-	 	boost::cell get_cell(std::size_t row, std::size_t column) {
-	 		return pimpl_->get_cell(row,column);
-	 	}
+		//! Gets the cell instance
+		//! which can be manipulated.
+		boost::cell get_cell(std::size_t row, std::size_t column) {
+			return pimpl_->get_cell(row,column);
+		}
 
 
 		//! Gets a row instance
@@ -88,36 +88,36 @@ namespace boost {
 		}
 
 		//! Gets the column instance
-	 	//! which can be manipulated.
-	 	//! No Exception Handling.
+		//! which can be manipulated.
+		//! No Exception Handling.
 		boost::column operator[](std::size_t i) {
 			return boost::column(pimpl_,i);
 		}
 
 		//! Gets the cell instance
-	 	//! which can be manipulated.
-	 	//! No Exception Handling.
+		//! which can be manipulated.
+		//! No Exception Handling.
 		boost::cell operator[](const std::string& str) {
 			std::size_t row = 0;
-		    std::size_t column = 0;
-		    for(auto &c : str) {
-		        if(!isdigit(c) && isupper(c)) {
-		            row = row*26 + (c - 'A' + 1);
-		        }
-		        else if(!isdigit(c) && islower(c)) {
-		            row = row*26 + (c - 'a'+ 1);
-		        }
-		        else {
-		                column = column*10 + (c - '0');
-		        }
-		    }
-		    // zero-indexed
-		    row -= 1;
-		    column -= 1;
+			std::size_t column = 0;
+			for(auto &c : str) {
+				if(!isdigit(c) && isupper(c)) {
+					row = row*26 + (c - 'A' + 1);
+				}
+				else if(!isdigit(c) && islower(c)) {
+					row = row*26 + (c - 'a'+ 1);
+				}
+				else {
+						column = column*10 + (c - '0');
+				}
+			}
+			// zero-indexed
+			row -= 1;
+			column -= 1;
 			return pimpl_->get_cell_unchecked(row,column);
 		}
-	 	
-	 	//! \brief Destructor
+		
+		//! \brief Destructor
 		//!        Closes Unsaved Documents.
 		~sheet() {
 		}
